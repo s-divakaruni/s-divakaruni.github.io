62e9b3ea291ed1a5e4a526e4c22e0a33e62086ef
s-divakaruni <shivani.divakaruni@gmail.com>
2025-07-25 21:11:38 -0400

    Full Stack project Updates

--- cti_110_node.js
+++ cti_110_node.js
@@ -30,11 +30,11 @@ app.use("/", router);
 
 router.get('/api/grades',function(req, res){
     pool.query(
-        `SELECT Students.student_id, first_name, last_name, AVG(assignments.grade) as total_grade \
-            FROM Students  \
-            LEFT JOIN Assignments ON Assignments.student_id = Students.student_id \
-            GROUP BY Students.student_id \
-            ORDER BY total_grade DESC`,
+    `SELECT students.student_id, first_name, last_name, AVG(assignments.grade) as total_grade \
+     FROM students \
+     LEFT JOIN assignments ON assignments.student_id = students.student_id \
+     GROUP BY students.student_id \
+     ORDER BY total_grade DESC`,
         [],
         function( err, result){
             if(err)
--- public/gradebook.js
+++ public/gradebook.js
@@ -1,17 +1,49 @@
-// TODO: Fetch data from the PostgreSQL database (to be implemented later)
 function fetchGradeData() {
-    // This function will query the PostgreSQL database and return grade data
     console.log("Fetching grade data...");
+
+    let xhr = new XMLHttpRequest();
+    let apiRoute = "/api/grades"; 
+
+    xhr.onreadystatechange = function () {
+        let results;
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                let data = JSON.parse(xhr.responseText);
+                populateGradebook(data);
+            } else {
+                console.error(`Could not get grades. 
+                    Status: ${xhr.status}`);
+            }
+            populateGradebook(JSON.parse(xhr.responseText));
+        }
+    };
+
+    xhr.open("GET", apiRoute, true);
+    xhr.send();
 }
 
 // TODO: Populate the table with grade data
 function populateGradebook(data) {
-    // This function will take the fetched grade data and populate the table
     console.log("Populating gradebook with data:", data);
+
+    let tableElm = document.getElementById("gradebook");
+
+    data.forEach(function (assignment) {
+        let row = document.createElement("tr");
+       let columns = [];
+       columns.name = document.createElement("td");
+       columns.name.appendChild(
+        document.createTextNode(assignment.last_name + " , " + assignment.first_name)
+       );
+       columns.grade = document.createElement("td");
+       columns.grade.appendChild(
+        document.createTextNode(assignment.total_grade)
+       );
+       row.appendChild(columns.name);
+       row.appendChild(columns.grade);
+       tableElm.appendChild(row)
+       
+    });
 }
 
-// TODO: REMOVE THIS
-// Call the stubs to demonstrate the workflow
-const gradeData = fetchGradeData();
-populateGradebook(gradeData);
-// END REMOVE
+fetchGradeData();
